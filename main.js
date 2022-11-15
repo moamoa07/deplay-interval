577d83a10eb24d696021f839c851fb7767802aa7
Moa Hedendahl <moahedendahl@gmail.com>
2022-11-15 09:54:57 +0100

    Add button to cancel background change

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 window.addEventListener("DOMContentLoaded", main);
 
+let timeoutRef;
+
 function main() {
     // start på programmet
 
-    setTimeout(changeBackgroundColor, 4000);
+    timeoutRef = setTimeout(changeBackgroundColor, 4000);
+    addEventListeners();
 }
 
 function changeBackgroundColor() {
     document.body.style.background = "lightblue";
+}
+
+function addEventListeners() {
+    const button = document.getElementById("cancel");
+    button.addEventListener("click", cancelBackgroundChange);
+}
+
+function cancelBackgroundChange() {
+    clearTimeout(timeoutRef);
 }
