a0a612cffee6de54ae1e174672c66118b98e750f
Moa Hedendahl <moahedendahl@gmail.com>
2022-11-15 10:55:52 +0100

    added button to start a random color background

--- main.js
+++ main.js
@@ -6,18 +6,49 @@ function main() {
     // start på programmet
 
     timeoutRef = setTimeout(changeBackgroundColor, 4000);
-    addEventListeners();
+    setupEventListeners();
 }
 
 function changeBackgroundColor() {
-    document.body.style.background = "lightblue";
+    document.body.style.background = getRandomColor();
 }
 
-function addEventListeners() {
-    const button = document.getElementById("cancel");
-    button.addEventListener("click", cancelBackgroundChange);
+function setupEventListeners() {
+    const cancelTimeoutButton = document.getElementById("cancel-timeout");
+    cancelTimeoutButton.addEventListener("click", cancelBackgroundChange);
+
+    const startIntervalButton = document.getElementById("start-interval");
+    startIntervalButton.addEventListener("click", startBackgroundChangeInterval);
 }
 
 function cancelBackgroundChange() {
     clearTimeout(timeoutRef);
+}
+
+function startBackgroundChangeInterval() {
+    setInterval(changeBackgroundColor, 1000);
+}
+
+function getRandomColor() {
+    const value = Math.random();
+
+    if(value < 0,2) {
+        return "lightgreen";
+    }
+
+    else if(value < 0,4) {
+        return "lightpink";
+    }
+
+    else if (value < 0,6) {
+        return "lavender";
+    }
+
+    else if (value < 0,8) {
+        return "lightyellow";
+    }
+
+    else {
+        return "lightred";
+    }
 }
