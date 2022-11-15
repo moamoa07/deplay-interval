92df09fb732888b1f52c68aa9b8d302333c8673a
Moa Hedendahl <moahedendahl@gmail.com>
2022-11-15 09:41:45 +0100

    add first files

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    // start på programmet
+
+    setTimeout(changeBackgroundColor, 4000);
+}
+
+function changeBackgroundColor() {
+    document.body.style.background = "lightblue";
+}
